9bbb6f146c7edd991dd1fc889d219f803e4dd63c
Ahmad Zia Bayat <ahmadzia_10@hotmail.com>
2018-03-29 22:22:32 -0400

    meeting app

---
+++ builds/angulardata/js/controllers/checkins.js
@@ -0,0 +1,23 @@
+myApp.controller('CheckInsController',
+    ['$scope','$rootScope','$routeParams', '$firebaseObject', '$firebaseArray',
+    function($scope, $rootScope, $routeParams, $firebaseObject, $firebaseArray) {
+
+      var ref;
+      $scope.whichmeeting = $routeParams.mId;
+      $scope.whichuser = $routeParams.uId;
+
+      ref = firebase.database().ref()
+      .child('users').child($scope.whichuser)
+      .child('meeting').child($scope.whichmeeting)
+      .child('checkins');
+
+      $scope.addCheckin = function(){
+        $firebaseArray(ref).$add({
+          firstname: $scope.user.firstname,
+          lastname: $scope.user.lastname,
+          email: $scope.user.email,
+          date: firebase.database.ServerValue.TIMESTAMP
+        });// $add
+      };// addCheckin
+
+}]); //myApp.controller
