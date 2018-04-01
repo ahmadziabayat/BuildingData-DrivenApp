02ae1946f91ba1cf60518d5df27f93aab4c65129
Ahmad Zia Bayat <ahmadzia_10@hotmail.com>
2018-04-01 17:59:54 -0400

    checkin page with his controller is added

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -1,8 +1,8 @@
 myApp.controller('CheckInsController',
-    ['$scope','$rootScope','$routeParams', '$firebaseObject', '$firebaseArray',
-    function($scope, $rootScope, $routeParams, $firebaseObject, $firebaseArray) {
+    ['$scope','$rootScope', '$location', '$routeParams', '$firebaseObject', '$firebaseArray',
+    function($scope, $rootScope, $location, $routeParams, $firebaseObject, $firebaseArray) {
 
-      var ref;
+      var ref, checkinsList;
       $scope.whichmeeting = $routeParams.mId;
       $scope.whichuser = $routeParams.uId;
 
@@ -11,13 +11,25 @@ myApp.controller('CheckInsController',
       .child('meeting').child($scope.whichmeeting)
       .child('checkins');
 
+      checkinsList = $firebaseArray(ref);
+      $scope.checkins = checkinsList;
+
       $scope.addCheckin = function(){
         $firebaseArray(ref).$add({
           firstname: $scope.user.firstname,
           lastname: $scope.user.lastname,
           email: $scope.user.email,
           date: firebase.database.ServerValue.TIMESTAMP
+        }).then(function(){
+          $location.path('/checkins/' + $scope.whichuser + '/' +
+          $scope.whichmeeting + '/checkinsList')
         });// $add
       };// addCheckin
+
+      $scope.deleteCheckin = function(id){
+        var refDel = ref.child(id);
+        var record = $firebaseObject(refDel);
+        record.$remove(id);
+      };
 
 }]); //myApp.controller
