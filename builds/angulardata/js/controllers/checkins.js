918373c72e41381e38ceb3d3870f8b67d66ec0d5
Ahmad Zia Bayat <ahmadzia_10@hotmail.com>
2018-04-03 22:15:15 -0400

    adding showing reward and deleting it from database

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -17,6 +17,37 @@ myApp.controller('CheckInsController',
       $scope.order = 'firstname';
       $scope.direction = 'asceding';
       $scope.query = '';
+      $scope.recordId='';
+
+      $scope.pickRandom = function(){
+        var whichRecord = Math.round(Math.random() * (checkinsList.length - 1));
+        $scope.recordId = checkinsList.$keyAt(whichRecord);
+      }; // pick random winner
+
+      $scope.showLove = function(myCheckin){
+        myCheckin.show = !myCheckin.show;
+        if(myCheckin.userState == 'expanded'){
+          myCheckin.userState ='';
+        }else {
+          myCheckin.userState = 'expanded';
+        }
+      }; // heart button to toggle
+
+      $scope.giveLove = function(myCheckin, myGift){
+        var refLove = ref.child(myCheckin.$id).child('awards');
+        var checkinsArray = $firebaseArray(refLove);
+
+        checkinsArray.$add({
+          name: myGift,
+          date: firebase.database.ServerValue.TIMESTAMP,
+        });
+      };
+      // delete awards function
+      $scope.deleteLove = function(myCheckin, key){
+        var refLove = ref.child(myCheckin.$id).child('awards').child(key);
+        var record = $firebaseObject(refLove);
+        record.$remove(key);
+      };
 
       $scope.addCheckin = function(){
         $firebaseArray(ref).$add({
